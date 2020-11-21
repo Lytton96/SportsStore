18bcfa6ac2bf0acfba94f4e7d03783bee95dfe7f
YL <YL>
2020-11-21 13:34:48 -0500

    1 [CHG] add a product page;

---
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vic.SportsStore.Domain.Abstract;
+using Vic.SportsStore.Domain.Concrete;
+
+namespace Vic.SportsStore.WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IProductsRepository productsRepository { get; set; }
+            = new InMemoryProductRepository();
+        public ViewResult List()
+        {
+            // M-V-C
+
+            // M -> ProductsRepository.Products
+            // V -> View
+            // C -> ProductController
+
+            return View(productsRepository.Products);
+        }
+
+    }
+}
